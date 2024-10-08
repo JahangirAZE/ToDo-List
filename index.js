68e0c00b7bde333a7795678ad3f1cbc7d83df561
Cahangir Əlisoy <156217652+JahangirAZE@users.noreply.github.com>
2024-10-08 23:49:21 +0400

    Update index.js

--- index.js
+++ index.js
@@ -1,32 +1,40 @@
 const todoList = [];
+
 function renderTodoList() {
   let todoListHTML = '';
   todoList.forEach((todoObject, index) => {
-    const {name, dueDate} = todoObject; //destructuring
+    const { name, dueDate, completed } = todoObject; // destructuring
+    const completedClass = completed ? 'completed' : '';
     const html = `
-      <div>${name}</div>
-      <div>${dueDate}</div>
-      <button class="delete-button js-delete-button">Delete</button>
+      <div class="todo-grid">
+        <div class="${completedClass}">${name}</div>
+        <div>${dueDate}</div>
+        <button class="delete-button js-delete-button" data-index="${index}">Delete</button>
+      </div>
     `;
     todoListHTML += html;
   });
   document.querySelector('.js-todo-list').innerHTML = todoListHTML;
-  document.querySelectorAll('.js-delete-button').
-    forEach((button, index) => {
-      button.addEventListener('click', () => {
-        todoList.splice(index, 1);
-        renderTodoList();
-      });
+
+  document.querySelectorAll('.js-delete-button').forEach((button) => {
+    button.addEventListener('click', (event) => {
+      const index = event.target.getAttribute('data-index');
+      todoList.splice(index, 1); // remove item
+      renderTodoList();
+    });
   });
 }
+
 document.querySelector('.js-add-todo-button').addEventListener('click', () => {
   addTodo();
 });
+
 function addTodo() {
-  const name = document.querySelector('.js-name-input').value;
+  const name = document.querySelector('.js-name-input').value.trim();
   const dueDate = document.querySelector('.js-due-date-input').value;
+
   if (name && dueDate) {
-    todoList.push({ name, dueDate });
+    todoList.push({ name, dueDate, completed: false });
     document.querySelector('.js-due-date-input').value = '';
     document.querySelector('.js-name-input').value = '';
     renderTodoList();
